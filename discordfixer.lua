1231e5929f4f01ad913ec47e4748f26b629e92ac
kode <105132310+kodescripts@users.noreply.github.com>
2023-06-27 18:04:48 +0200

    Create discordfixer.lua

---
+++ discordfixer.lua
@@ -0,0 +1,5 @@
+for _,v in pairs(game:GetService("CoreGui"):GetDescendants()) do
+    if v:IsA("TextLabel") and v.Text == "discord.gg/BPBcCutZzd" then
+        v.Text = "discord.gg/cwV4H6WQrh" -- the dizzy link expired wtf :sob:
+    end
+end
